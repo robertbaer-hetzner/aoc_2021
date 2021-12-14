bf335634e3c8025719b51c4a63c825a16a6538e3
robbaer96 <baer.robert96@gmx.de>
2021-12-15 00:27:15 +0100

    works

--- day7/day7_p1.py
+++ day7/day7_p1.py
@@ -4,19 +4,25 @@ import numpy as np
 
 def get_input():
     fish_timer = []
-    with open('C:\\Git\\aoc_2021\\day7\\input.txt', 'r') as file:
+    with open('E:\\Git\\aoc_2021\\day7\\input.txt', 'r') as file:
         for line in file:
             fish_timer += line.split(',')
     ar = np.array(fish_timer, int) 
     return ar
 
-
-
 #START
 if __name__ == "__main__":
     numberlist = get_input().astype('int')
+    store_value = sum(numberlist)
+    new_value = 0
+    counter = 0
+    while counter < ( max(numberlist) - min(numberlist) ):
+        numberlist_2 = []
+        for element in numberlist:
+            numberlist_2.append(abs(element - counter))
+        new_value = sum(numberlist_2)
+        if new_value < store_value:
+            store_value = new_value  
+        counter += 1
 
-    print(min(numberlist))
-    print(max(numberlist))
-
-    
+    print('The minimal fuel costs are: ',store_value)
